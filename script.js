b41162ff82eaac48fbf68e5aba9d0525ab9cdc64
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-06-20 10:41:47 +0100

    Fourteenth Commit: Implement getMealsBySearch() function

--- script.js
+++ script.js
@@ -28,3 +28,12 @@ async function getMealById(id) {
 
   return meal;
 }
+
+async function getMealsBySearch(term) {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
+  const respData = await resp.json();
+  const meals = respData.meals;
+  return meals;
+}
