8ba6ca9caa5ac501c6f5afeb3f788758ccda5149
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-06-20 10:40:42 +0100

    Thirteenth Commit: Implement getMealById() function

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ async function getRandomMeal() {
   const randomMeal = respData.meals[0];
   addMeal(randomMeal, true);
 }
+
+async function getMealById(id) {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+  const respData = await resp.json();
+  const meal = respData.meals[0];
+
+  return meal;
+}
